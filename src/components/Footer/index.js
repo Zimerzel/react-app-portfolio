58d38dd189271dbef79b57d08a55fef6485592b0
Imerzel <zach.imerzel@gmail.com>
2021-07-30 11:08:52 -0500

    added base footer layout

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {   
+    return (
+    <div class="footer" id="footer">
+        <a href=""><img src="" alt="Github" class="icon"/></a>
+        <a href=""><img src="" alt="LinkedIn" class="icon"/></a>
+        <p>
+            &copy; Zach Imerzel |  <a href="" class="link">download resume</a>
+        </p>
+
+    </div>
+    )
+}
+
+export default Footer;
