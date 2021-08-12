30ec6a8967b74c604a6563e87592954fd496bb45
Imerzel <zach.imerzel@gmail.com>
2021-08-12 18:40:10 -0500

    added resume download

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ function Footer() {
         {/* <a href=""><img src="" alt="Github" class="icon"/></a>
         <a href=""><img src="" alt="LinkedIn" class="icon"/></a> */}
         <p id ="footer-txt">
-            &copy; Zach Imerzel | <a href="https://www.linkedin.com/in/zach-imerzel-336b9414b/">| LinkedIn |</a> <a href="https://github.com/Zimerzel">| Github |</a>  <a href="" class="link">| Download Resume </a>
+            &copy; Zach Imerzel | <a href="https://www.linkedin.com/in/zach-imerzel-336b9414b/">| LinkedIn |</a> <a href="https://github.com/Zimerzel">| Github |</a>  <a href="/images/resume.jpg" download>| Download Resume </a>
         </p>
 
     </div>
