86cdd7b6d9ff318607694b0948fbb6a58b0fb391
Imerzel <zach.imerzel@gmail.com>
2021-08-13 16:03:30 -0500

    updated all links to open new page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,10 +7,10 @@ function Footer() {
         <a href=""><img src="" alt="LinkedIn" class="icon"/></a> */}
       <p id="footer-txt">
         &copy; Zach Imerzel |{" "}
-        <a href="https://www.linkedin.com/in/zach-imerzel-336b9414b/">
+        <a href="https://www.linkedin.com/in/zach-imerzel-336b9414b/" target="_blank">
           | LinkedIn |
-        </a>{" "}
-        <a href="https://github.com/Zimerzel">| Github |</a>{" "}
+        </a>
+        <a href="https://github.com/Zimerzel" target="_blank">| Github |</a>{" "}
         <a href= "https://docdro.id/g8dL6kd" target="_blank" download>
           | Download Resume
         </a>
