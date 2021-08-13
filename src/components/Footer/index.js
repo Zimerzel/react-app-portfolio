ee50a091dd284145acfa37b67798b95e6bb711ab
Imerzel <zach.imerzel@gmail.com>
2021-08-13 14:32:09 -0500

    fixed download link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React from "react";
 
-function Footer() {   
-    return (
+function Footer() {
+  return (
     <div className="footer" id="footer">
-        {/* <a href=""><img src="" alt="Github" class="icon"/></a>
+      {/* <a href=""><img src="" alt="Github" class="icon"/></a>
         <a href=""><img src="" alt="LinkedIn" class="icon"/></a> */}
-        <p id ="footer-txt">
-            &copy; Zach Imerzel | <a href="https://www.linkedin.com/in/zach-imerzel-336b9414b/">| LinkedIn |</a> <a href="https://github.com/Zimerzel">| Github |</a>  <a href="/images/resume.jpg" download>| Download Resume<img src="/images/resume.jpg" alt="resume-img" id="img-download"/></a>
-        </p>
-
+      <p id="footer-txt">
+        &copy; Zach Imerzel |{" "}
+        <a href="https://www.linkedin.com/in/zach-imerzel-336b9414b/">
+          | LinkedIn |
+        </a>{" "}
+        <a href="https://github.com/Zimerzel">| Github |</a>{" "}
+        <a href= "https://docdro.id/g8dL6kd" download>
+          | Download Resume
+        </a>
+      </p>
     </div>
-    )
+  );
 }
 
 export default Footer;
